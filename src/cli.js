d2ada223d1273cecb623a87195a5bccd3589cafc
Rafael Ramalho <rafazelramalho19@gmail.com>
2019-10-17 19:19:52 +0100

    chore: started the cli base

---
+++ src/cli.js
@@ -0,0 +1,30 @@
+import arg from 'arg';
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            '--git': Boolean,
+            '--yes': Boolean,
+            '--install': Boolean,
+            '-g': '--git',
+            '-y': '--yes',
+            '-i': '--install',
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+
+    return {
+        skipPrompts: args['--yes'] || false,
+        git: args['--git'] || false,
+        template: args._[0],
+        runInstall: args['--install'] || false,
+    };
+}
+
+export function cli(args) {
+    const options = parseArgumentsIntoOptions(args);
+
+    console.log(options);
+}
---
+++ src/index.js
@@ -0,0 +1,1 @@
+require('esm')(module)('../src/cli').cli(process.argv);
